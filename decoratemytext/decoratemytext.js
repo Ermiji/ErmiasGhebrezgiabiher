c82876f5a9077a86d4d83e0c8e043a6d0c2d1548
Ermias <eghebrez@gmail.com>
2022-01-16 21:25:32 -0500

    Decorate excercise.

--- decoratemytext/decoratemytext.js
+++ decoratemytext/decoratemytext.js
@@ -17,11 +17,60 @@ function verifyMsg(){
         document.getElementById("ta").style.fontWeight = "bold";
         document.getElementById("ta").style.color = "green";
         document.getElementById("ta").style.textDecoration = "underline";
-        document.body.style.backgroundImage = "url('')";
+        document.body.style.backgroundImage = "url('http://www.cs.washington.edu/education/courses/190m/CurrentQtr/labs/6/hundred-dollar-bill.jpg')";
     }else{
         document.getElementById("ta").style.fontWeight = "normal";
         document.getElementById("ta").style.color = "black";
         document.getElementById("ta").style.textDecoration = "none";
         document.body.style.backgroundImage = "url('')";
     }
+}
+
+function pigLatin1(){
+    var text = document.getElementById("ta").value;
+    text = text.trim();
+    let textArray = text.split(" ");
+    var rslt = "";
+    for(var i = 0; i < textArray.length; i++){
+        rslt += pigLatin(textArray[i]) + " ";
+    }
+    rslt = rslt.trim();
+    document.getElementById("ta").value = rslt;
+}
+
+function pigLatin(input){
+    //var txt = document.getElementById("ta").value;
+    var char = input.charAt(0);
+    var rslt = "";
+    if( (char == 'a') || (char == 'e') || (char == 'i') || (char == 'o') || (char == 'u')){
+        rslt = input + "ay";
+    }else{
+        var cnsnnt = 0;
+        for(var i = 0; i < input.length; i++){
+            char = input.charAt(i);
+            if( (char == 'a') || (char == 'e') || (char == 'i') || (char == 'o') || (char == 'u')){
+                break;
+        }else{
+            cnsnnt++;
+        }
+    }
+    rslt = input.substring(cnsnnt) + input.substring(0, cnsnnt) + "ay";
+   }
+  return rslt;
+}
+
+function malkovitch(){
+    var txt = document.getElementById("ta").value;
+    txt = txt.trim();
+    let textArray = txt.split(" ");
+    var rslt = "";
+    for(var i = 0; i < textArray.length; i++){
+        if(textArray[i].length >= 5){
+            rslt += "Malkovitch" + " ";
+        }else{
+            rslt += textArray[i] + " ";
+        }
+    }
+    rslt = rslt.trim();
+    document.getElementById("ta").value = rslt;
 }
